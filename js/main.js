d3f4f068815a6c14741e234e95c0d902cd7c758e
frank rowe <fsrowe@gmail.com>
2013-01-02 15:31:00 -0500

    removed esrgc base layer

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 
   var map = new L.Map('map', {attributionControl: false}).setView(new L.LatLng(38.35, -76), 8);
 
-  var mapboxURL = 'http://a.tiles.mapbox.com/v3/esrgc.map-0y6ifl91/{z}/{x}/{y}.png';
+  var mapboxURL = 'http://a.tiles.mapbox.com/v3/{{YOUR-MAP-ID}}/{z}/{x}/{y}.png';
   L.tileLayer(mapboxURL, { attribution: 'MD iMAP'}).addTo(map);
 
 });
