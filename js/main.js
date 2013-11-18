b82a5034665cf351e16a05593712974ad4ce68f4
Frank Rowe <fsrowe@gmail.com>
2013-11-18 16:22:46 -0500

    spaces yo

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
   var map = new L.Map('map').setView(new L.LatLng(38.85, -77), 7);
 
   L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
   }).addTo(map);
 
 });
